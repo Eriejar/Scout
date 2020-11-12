544f99b64fe36f41a1e6b85c5ffc0f532a182f04
kellybw12 <72891670+kellybw12@users.noreply.github.com>
2020-11-11 20:11:49 -0800

    Add files via upload
    Added multi-agent implementation

--- bin/ScoutAI/Kelly_ScoutAI.py
+++ bin/ScoutAI/Kelly_ScoutAI.py
@@ -8,6 +8,7 @@ except:
 import os
 import sys
 import time
+import malmoutils
 import json
 import random
 from tqdm import tqdm
@@ -22,9 +23,46 @@ from torch.utils.data import Dataset, DataLoader
 
 from ScoutAI import ScoutAI 
 
-SIZE = 50
+SIZE = 10
 OBS_SIZE = 5
 MAX_EPISODE_STEPS = 100
+
+MalmoPython.setLogging("", MalmoPython.LoggingSeverityLevel.LOG_OFF)
+
+def safeStartMission(agent_host, my_mission, my_client_pool, my_mission_record, role, expId):
+    print("Starting Mission {}.".format(role))
+    max_retries = 5    
+    for retry in range(max_retries):
+        try:
+            agent_host.startMission(my_mission, my_client_pool, my_mission_record, role, expId)
+            break
+        except RuntimeError as e:
+            if retry == max_retries - 1:
+                print("Error starting mission:", e)
+                exit(1)
+            else:
+                time.sleep(2)
+
+def safeWaitForStart(agent_hosts):
+    start_flags = [False for a in agent_hosts]
+    start_time = time.time()
+    time_out = 120  # Allow a two minute timeout.
+    while not all(start_flags) and time.time() - start_time < time_out:
+        states = [a.peekWorldState() for a in agent_hosts]
+        start_flags = [w.has_mission_begun for w in states]
+        errors = [e for w in states for e in w.errors]
+        if len(errors) > 0:
+            print("Errors waiting for mission start:")
+            for e in errors:
+                print(e.text)
+            exit(1)
+        time.sleep(0.1)
+        print(".", end=' ')
+    if time.time() - start_time >= time_out:
+        print("Timed out while waiting for mission to start.")
+        exit(1)
+    print()
+    print("Mission has started.")
 
 def buildEnvironment():
 
@@ -49,10 +87,11 @@ def buildEnvironment():
                             "<DrawCuboid x1='{}' x2='{}' y1='2' y2='2' z1='{}' z2='{}' type='air'/>".format(-SIZE, SIZE, -SIZE, SIZE) + \
                             "<DrawCuboid x1='{}' x2='{}' y1='1' y2='1' z1='{}' z2='{}' type='stone'/>".format(-SIZE, SIZE, -SIZE, SIZE) + \
                             '''<DrawBlock x='0'  y='2' z='0' type='air' />
-                            <DrawBlock x='0' y='1' z='1' type='redstone_block' />
+                            <DrawBlock x='5' y='1' z='5' type='redstone_block' />
                             <DrawBlock x='0' y='1' z='0' type='emerald_block' />
-                            <DrawEntity x='1' y='2' z='2' type="Cow" />
+                            <DrawEntity x='1' y='2' z='10' type="Cow" />
                             <DrawEntity x='10' y='2' z='20' type="Wolf" />
+                            <DrawBlock x='8' y='2' z='10' type='diamond_block' />
                         </DrawingDecorator>
                         <ServerQuitWhenAnyAgentFinishes/>
                     </ServerHandlers>
@@ -60,6 +99,34 @@ def buildEnvironment():
 
                 <AgentSection mode="Survival">
                     <Name>Trainer</Name>
+                    <AgentStart>
+                        <Placement x="5.5" y="2" z="5.5" pitch="0" yaw="90"/>
+                        <Inventory>
+                            <InventoryItem slot="0" type="diamond_pickaxe"/>
+                        </Inventory>
+                    </AgentStart>
+                    <AgentHandlers>
+                        <DiscreteMovementCommands/>
+                        <ObservationFromFullStats/>
+                        <RewardForCollectingItem>
+                            <Item type="diamond" reward="1.0"/>
+                        </RewardForCollectingItem>
+                        <RewardForTouchingBlockType>
+                            <Block reward="-1.0" type="lava"/>
+                        </RewardForTouchingBlockType>
+                        <ObservationFromGrid>
+                            <Grid name="floorAll">
+                                <min x="-10" y="-1" z="-10"/>
+                                <max x="10" y="-1" z="10"/>
+                            </Grid>
+                        </ObservationFromGrid>
+                        <AgentQuitFromReachingCommandQuota total="'''+str(MAX_EPISODE_STEPS)+'''" />
+                    </AgentHandlers>
+                </AgentSection> 
+
+                
+                <AgentSection mode="Survival">
+                    <Name>Scout</Name>
                     <AgentStart>
                         <Placement x="0.5" y="2" z="0.5" pitch="45" yaw="0"/>
                         <Inventory>
@@ -77,42 +144,58 @@ def buildEnvironment():
                         </RewardForTouchingBlockType>
                         <ObservationFromGrid>
                             <Grid name="floorAll">
-                                <min x="-'''+str(int(OBS_SIZE/2))+'''" y="-1" z="-'''+str(int(OBS_SIZE/2))+'''"/>
-                                <max x="'''+str(int(OBS_SIZE/2))+'''" y="0" z="'''+str(int(OBS_SIZE/2))+'''"/>
+                                <min x="-10" y="-1" z="-10"/>
+                                <max x="10" y="-1" z="10"/>
                             </Grid>
                         </ObservationFromGrid>
                         <AgentQuitFromReachingCommandQuota total="'''+str(MAX_EPISODE_STEPS)+'''" />
                     </AgentHandlers>
                 </AgentSection> 
+
             </Mission>''' 
 
-                # <AgentSection mode="Survival">
-                #     <Name>''' + "Scout" + '''</Name>
-                #     <AgentStart>
-                #       <Placement x="5" y="2" z="5" pitch="45" yaw="0"/>
-                #       <Inventory>
-                #         <InventoryObject type="wooden_pickaxe" slot="0" quantity="1"/>
-                #       </Inventory>
-                #     </AgentStart>
-                #     <AgentHandlers>
-                #       <ContinuousMovementCommands turnSpeedDegs="360"/>
-                #       <ChatCommands/>
-                #       <MissionQuitCommands/>
-                #       <ObservationFromNearbyEntities>
-                #         <Range name="entities" xrange="40" yrange="2" zrange="40"/>
-                #       </ObservationFromNearbyEntities>
-                #       <ObservationFromRay/>
-                #       <ObservationFromFullStats/>
-                #     </AgentHandlers>
-                #     </AgentSection>
-            
+def identify_command(command):
+    if command == "thumbs up":
+        return "move 1"
+
+    if command == "1":
+        return "turn 1"
+
+    if command == "2":
+        return "attack 1"    
+
+    if command == "thumbs down":
+        return "quit"    
 
 
 
 if __name__ == '__main__':
     # Create default Malmo objects:
     agent_host = MalmoPython.AgentHost()
-    scout_ai = ScoutAI(agent_host)
+    #scout_ai = ScoutAI(agent_host)
+    scout_ai = MalmoPython.AgentHost()
+    malmoutils.parse_command_line(agent_host)
+
+    my_mission = MalmoPython.MissionSpec(buildEnvironment(),True)
+    client_pool = MalmoPython.ClientPool()
+    client_pool.add(MalmoPython.ClientInfo( "127.0.0.1", 10000) )
+    client_pool.add(MalmoPython.ClientInfo( "127.0.0.1", 10001) )
+    agent_host_record = MalmoPython.MissionRecordSpec()
+    scout_record = MalmoPython.MissionRecordSpec()
+
+    safeStartMission(agent_host, my_mission, client_pool, agent_host_record,0,'')
+    safeStartMission(scout_ai, my_mission, client_pool, scout_record,1,'')
+
+    safeWaitForStart([agent_host,scout_ai])
+
+    while agent_host.peekWorldState().is_mission_running or scout_ai.peekWorldState().is_mission_running:
+        
+        command = input("Enter a command please: ")
+        scout_ai.sendCommand(identify_command(command))
+        time.sleep(0.5)
+    print("Mission end")
+    exit(1)
+    '''
     try:
         agent_host.parse( sys.argv )
     except RuntimeError as e:
@@ -122,18 +205,24 @@ if __name__ == '__main__':
     if agent_host.receivedArgument("help"):
         print(agent_host.getUsage())
         exit(0)
-    #client_pool = MalmoPython.ClientPool()
-    #client_pool.add(MalmoPython.ClientInfo( "127.0.0.1", 10000) )
-    #client_pool.add(MalmoPython.ClientInfo( "127.0.0.1", 10001) )
+    role = 0
+    print(sys.argv)
+    if len(sys.argv) >= 2:
+        role = sys.argv[3]
+        print("Will run as role", role)
+    client_pool = MalmoPython.ClientPool()
+    client_pool.add(MalmoPython.ClientInfo( "127.0.0.1", 10000) )
+    client_pool.add(MalmoPython.ClientInfo( "127.0.0.1", 10001) )
     
     my_mission = MalmoPython.MissionSpec(buildEnvironment(),True)
     my_mission_record = MalmoPython.MissionRecordSpec()
-
+   
+    
     # Attempt to start a mission:
     max_retries = 3
     for retry in range(max_retries):
         try:
-            agent_host.startMission( my_mission, my_mission_record )
+            agent_host.startMission( my_mission, client_pool, my_mission_record, int(role), "MMExp#1" )
             break
         except RuntimeError as e:
             if retry == max_retries - 1:
@@ -157,15 +246,20 @@ if __name__ == '__main__':
 
 
     scout_ai.go_to_redstone_block(world_state)
+    action_index = 0
+    numActions = len(scout_ai.action_stack)
     # Loop until mission ends:
-    while True:
-    #while world_state.is_mission_running:
-        print(".", end="")
+    while world_state.is_mission_running:
+        #print(".", end="")
         time.sleep(0.1)
+        if len(scout_ai.action_stack) == 0:
+            time.sleep(2)
+        else:
+            agent_host.sendCommand(scout_ai.get_next_action())
+        action_index += 1
 
-        
-        action = scout_ai.move_south()
-        agent_host.sendCommand(action)
+        if numActions == action_index:
+            time.sleep(2)
 
         world_state = agent_host.getWorldState()
         for error in world_state.errors:
@@ -174,7 +268,7 @@ if __name__ == '__main__':
     print()
     print("Mission ended")
     # Mission has ended.
-    
-    
-
+  
+    '''
     #train(agent_host)
+    
